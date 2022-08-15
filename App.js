5259a692ecf2dd2f732e5e5be96301200a60eb2b
KCynide <jyl5099@gmail.com>
2022-08-16 00:53:24 +0900

    로그인창 구현_1
    로고는 아무거나 씀(대만 녹색당?이라고 함)

--- App.js
+++ App.js
@@ -1,19 +1,100 @@
 import React, {useState} from 'react';
 import {View, StatusBar} from 'react-native';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import './components/login.js'
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
-const App = () => {
-  return(
 
+//import MainScreen from './MainScreen';
+//import DetailScreen from './DetailScreen';
+
+ 
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("./assets/logo-greenery.png")} />
+ 
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Creat Account</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
   );
-};
-
+}
+ 
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: ' #88b04b',
-  }
+    backgroundColor: '#88b04b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image :{
+    marginBottom: 40
+ 
+  },
+  inputView: {
+    backgroundColor: "#C3FC6d",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "flex-start",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10
+  },
+ 
+  forgot_button: {
+    height: 30,
+    marginBottom: 10,
+  },
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#577030",
+  },
 });
-
-export default App;
